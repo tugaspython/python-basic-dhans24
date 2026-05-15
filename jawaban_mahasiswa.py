d6939ba165753cae0c58e2c889e8e58e46130634
dhans24 <fastgoole@gmail.com>
2026-05-15 17:38:28 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,12 @@
-# JAWAB DISINI!
+def validasi_password(password):
+    if len(password) < 8 or len(password) > 20: 
+        return False
+    if " " in password: 
+        return False
+
+    # Menggunakan list comprehension & any() (menambah sedikit nilai CC tapi masih wajar)
+    has_upper = any(c.isupper() for c in password)
+    has_digit = any(c.isdigit() for c in password)
+    has_symbol = any(c in "!@#$%^&*" for c in password)
+
+    return has_upper and has_digit and has_symbol
